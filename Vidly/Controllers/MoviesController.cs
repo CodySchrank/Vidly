940482e17f3bb6fc141892c2d35b3c4fdb311cd2
Rogue <Rogue@CODYSCHRANKAE18>
2017-08-10 18:41:16 -0700

    Finished movie controller

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -35,25 +35,24 @@ namespace Vidly.Controllers
             return View(movie);
         }
 
-        // GET: Movies/Random
-        public ActionResult Random()
-        {
-            var movie = new Movie() { Name = "Shrek!" };
-
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie
-            };
-
-            return View(viewModel);
-        }
-
         public ActionResult Save(Movie movie)
         {
-            movie.CreatedAt = DateTime.Now;            
-            _context.Movies.Add(movie);
-            _context.SaveChanges();
+            if (movie.Id == 0)
+            {
+                movie.CreatedAt = DateTime.Now;
+                _context.Movies.Add(movie);
+            }
+            else
+            {
+                var movieInDb = _context.Movies.Single(m => m.Id == movie.Id);
 
+                movieInDb.Name = movie.Name;
+                movieInDb.ReleaseDate = movie.ReleaseDate;
+                movieInDb.Stock = movie.Stock;
+                movieInDb.GenreId = movie.GenreId;
+            }
+
+            _context.SaveChanges();
             return RedirectToAction("Index", "Movies");
         }
 
@@ -64,6 +63,34 @@ namespace Vidly.Controllers
             var movieFormViewModel = new MovieFormViewModel()
             {
                 Genres = genres
+            };
+
+            return View("MovieForm", movieFormViewModel);
+        }
+
+
+        // GET: Movies/Random
+        public ActionResult Random() {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            var viewModel = new RandomMovieViewModel {
+                Movie = movie
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            var movieFormViewModel = new MovieFormViewModel()
+            {
+                Movie = movie,
+                Genres = _context.Genres.ToList()
             };
 
             return View("MovieForm", movieFormViewModel);
